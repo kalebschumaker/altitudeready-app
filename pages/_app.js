f292aac4d69955cdb7f8c9a3771cd041e14f84fa
kalebschumaker <kalebschumaker@gmail.com>
2026-01-05 13:45:26 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,17 @@
+import { Amplify } from 'aws-amplify';
+import { cognitoConfig } from '../lib/config';
+
+// Configure Amplify
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: cognitoConfig.userPoolId,
+      userPoolClientId: cognitoConfig.userPoolClientId,
+      region: cognitoConfig.region
+    }
+  }
+});
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
