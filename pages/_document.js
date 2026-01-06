08135624ea66c66e4f792d52745bf78981cc6e54
kalebschumaker <kalebschumaker@gmail.com>
2026-01-06 13:18:12 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,14 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXXX"
           crossOrigin="anonymous"
         />
+                    {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        
+        {/* Optional: Theme color for mobile browsers */}
+        <meta name="theme-color" content="#2563eb" />
       </Head>
       <body>
         <Main />
